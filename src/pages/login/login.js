a871d5446e4c34fa7bfc643e3c0cd4ccdf2bfcdb
Jefferson <jeffersonacm.jz@gmail.com>
2019-08-10 15:26:04 -0300

    user login implemented and css styles

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import './login.css';
+import React, { useState } from 'react';
+import './Login.css';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
-export default function login() {
+export default function Login({ history }) {
+    const [username, setUsername] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username,
+        });
+
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`);
+    }
+
     return(
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="logoLogin" />
-                <input placeholder="Digite seu usuario do GitHub" />
+                <input
+                    placeholder="Digite seu usuario do GitHub"
+                    value= {username}
+                    onChange={e => setUsername(e.target.value)}
+                 />
                 <button type="submit">Entrar</button>
             </form>
         </div>
     );
 }
-
- 
